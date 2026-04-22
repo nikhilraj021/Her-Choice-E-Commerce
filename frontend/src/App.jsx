8a3e38cf8282629694cb2fcdb55fa1e68098146f
nikhilraj021 <dnraj2001@gmail.com>
2026-04-22 15:09:51 +0530

    feat: add About, Collection, Contact, and Home pages with basic structure refactor: standardize JSX formatting in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div className='text-3xl font-bold'>Her-Choice E-Commerce Application</div>
-  )
-}
+    <div className="text-3xl font-bold">Her-Choice E-Commerce Application</div>
+  );
+};
 
-export default App
+export default App;
